3986885244b1d0b797ef953281528e15ee2b33a4
Maarten Hofmans <82232293+mhofmans@users.noreply.github.com>
2024-04-17 10:24:40 +0200

    Added example script
    This is an working example script for p1monitor to read 3 phases and send to zabbix.

---
+++ Examples/zbxmqtt_phase_p1monitor.py
@@ -0,0 +1,38 @@
+import paho.mqtt.client as mqtt
+from pyzabbix import ZabbixMetric, ZabbixSender
+
+ZabbixServer="172.28.3.150" # zabbix ip
+ZabbixPort=10051 # zabbix listening port default 10050
+Broker="172.28.3.152" # broker ip
+
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, reason_code, properties):
+    print(f"Connected with result code {reason_code}")
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("p1monitor/phase/l1_v")
+    client.subscribe("p1monitor/phase/l2_v")
+    client.subscribe("p1monitor/phase/l3_v")
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+    packet = [
+        ZabbixMetric(str(msg.topic).split('/')[1], str(msg.topic).split('/')[2], str(msg.payload.decode('utf-8'))),
+    ]
+
+    result = ZabbixSender(zabbix_server=ZabbixServer, zabbix_port=ZabbixPort, use_config=None, chunk_size=250).send(packet)
+    print(result)
+
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+
+mqttc.connect(Broker, 1883, 60) # broker ip
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+mqttc.loop_forever()
